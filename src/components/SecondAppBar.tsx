324863be0a7c4e1d3c2d8450eb5d9fa465163604
vibhaw raj <vibhawraj494@gmail.com>
2024-06-27 13:03:00 +0530

    [fix]

--- src/components/SecondAppBar.tsx
+++ src/components/SecondAppBar.tsx
@@ -7,19 +7,16 @@ const SecondAppBar: React.FC<SecondAppBarProps> = () => {
   return (
     <div className='border-b-2 border-[#E7E7E7]'>
  <div className="flex flex-row items-center p-4 text-center   px-4 gap-10 h-[67px] ml-24">
-      <div className="font-bold  items-center align-middle  text-center  font-sans text-[#DC4A2D]  cursor-pointer">
+      <div className="font-semibold  items-center align-middle  text-center  font-sans text-[#DC4A2D] relative border-b-2 pb-4 cursor-pointer text-xl leading-7 justify-center mt-5 border-[#DC4A2D] ">
         Job Preview
-        <div className='border-b-2'>
-
-        </div>
       </div>
-      <div className="ml-10 font-sans text-[#888888] cursor-pointer">
+      <div className="ml-10 font-sans text-[#888888] cursor-pointer text-xl leading-7 font-medium">
         Applicants
       </div>
-      <div className="ml-10 font-sans text-[#888888] cursor-pointer">
+      <div className="ml-10 font-sans text-[#888888] cursor-pointer text-xl leading-7 font-medium">
         Match
       </div>
-      <div className="ml-10 font-sans text-[#888888] cursor-pointer">
+      <div className="ml-10 font-sans text-[#888888] cursor-pointer text-xl leading-7 font-medium">
         Messages
       </div>
     </div>
